4a3cc207c79f41b3dc7447c6ea6aeb70086c7231
Michael Hengge <mi.hengge@googlemail.com>
2025-01-09 10:08:38 +0100

    Change docstring to google type

--- Magician.py
+++ Magician.py
@@ -10,10 +10,18 @@ class DobotMagicianE6:
         self.isDebug = True
 
     def Connect(self):
-        '''
+        """
         Connect to the Dobot Magician E6 robot.
-        :return: None
-        '''
+
+        Args:
+            None
+        
+        Returns:
+            None
+        
+        Raises:
+            Exception: If the connection fails.
+        """
         try :
             if self.isDebug: print(f"Connecting to Dobot at {self.ip}:{self.port}...")
             self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,10 +35,18 @@ class DobotMagicianE6:
             self.connection = None
 
     def EnableRobot(self):
-        '''
+        """
         Enable the Dobot Magician E6 robot.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            None
+
+        Returns:
+            The response from the robot.
+        
+        Raises:
+            Exception: If the control mode is not TCP.
+        """
         if self.isEnabled == False:
             if self.isDebug: print("  Enabling Dobot Magician E6...")
             response = self.Send_command("EnableRobot()")
@@ -42,10 +58,15 @@ class DobotMagicianE6:
                 return response
 
     def DisableRobot(self):
-        '''
+        """
         Disable the Dobot Magician E6 robot.
-        :return: The response from the robot.
-        '''
+        
+        Args:
+            None
+
+        Returns:
+            The response from the robot.
+        """
         if self.isEnabled:
             response = self.Send_command("DisableRobot()")
             self.isEnabled = False
@@ -53,25 +74,46 @@ class DobotMagicianE6:
             return response
 
     def Disconnect(self):
+        """
+        Disconnect from the Dobot Magician E6 robot.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         if self.connection:
             self.connection.close()
             self.connection = None
             if self.isDebug: print("  Disconnected from Dobot Magician E6")
 
     def ClearError(self):
-        '''
+        """
         Clear any errors on the Dobot Magician E6 robot.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            None
+
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print("  Clearing Dobot Magician E6 errors...")
         return self.Send_command("ClearError()")
 
     def Send_command(self, command):
-        '''
+        """
         Send a command to the Dobot and receive a response.
-        :param command: The command string to send.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            command (string): The command to send to the robot.
+
+        Returns:
+            The response from the robot.
+
+        Raises:
+            Exception: If not connected to the Dobot Magician E6.
+        """
         if self.connection:
             try:
                 self.connection.sendall(command.encode() + b'\n')
@@ -84,60 +126,80 @@ class DobotMagicianE6:
             raise Exception("  ! Not connected to Dobot Magician E6")
 
     def MoveJ(self,j1,j2,j3,j4,j5,j6):
-        '''
+        """
         Move the robot to a specified joint position.
-        :param j1: The joint 1 position.
-        :param j2: The joint 2 position.
-        :param j3: The joint 3 position.
-        :param j4: The joint 4 position.
-        :param j5: The joint 5 position.
-        :param j6: The joint 6 position.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            j1 (int): Joint 1 angle.
+            j2 (int): Joint 2 angle.
+            j3 (int): Joint 3 angle.
+            j4 (int): Joint 4 angle.
+            j5 (int): Joint 5 angle.
+            j6 (int): Joint 6 angle.
+
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print(f"  Joint move robot to ({j1},{j2},{j3},{j4},{j5},{j6})")
         move_command = f"MovJ(joint={{{j1},{j2},{j3},{j4},{j5},{j6}}})"
         return self.Send_command(move_command)
     
     def Home(self):
-        '''
+        """
         Move the robot to the home position.
-        :return: The response from the robot.
-        '''
+
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print("  Moving robot to home position")
         return self.MoveJ(0,0,0,0,0,0)
 
     def SetDebug(self, isDebug):
-        '''
+        """
         Set the debug mode for the Dobot Object
-        :param isDebug: Print Debug messages yes (True) or no  (False).
-        :return: None
-        '''
+
+        Args
+        isDebug (bool): Print Debug messages yes (True) or no  (False).
+
+        Returns:
+            None
+        """
         self.isDebug = isDebug
 
     def ToolDO(self, index, status):
-        '''
+        """
         Set the digital output of the tool.
-        :param index: Tool DO index.
-        :param status: Tool DO status. 1: ON, 0: OFF.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            index (int): Tool DO index (1 or 2).
+            status (int): Tool DO status. 1: ON, 0: OFF.
+
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print(f"  Setting tool digital output pin {index} to {status}")
         return self.Send_command(f"ToolDO({index},{status})")
     
     def ClearError(self):
-        '''
+        """
         Clear any errors on the Dobot Magician E6 robot.
-        :return: The response from the robot.
-        '''
+
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print("  Clearing Dobot Magician E6 errors...")
         return self.Send_command("ClearError()")
 
     def SetSucker(self, status):
-        '''
+        """
         Set the sucker status.
-        :param status: Sucker status. 1: ON, 0: OFF.
-        :return: The response from the robot.
-        '''
+
+        Args:
+            status (int): Sucker status. 1: ON, 0: OFF.
+        
+        Returns:
+            The response from the robot.
+        """
         if self.isDebug: print(f"  Setting sucker to {status}")
         return self.ToolDO(1,status)
 
