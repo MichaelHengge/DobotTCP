fbe22742d2a6d11ffd4203eb363e362b2fd8823f
Michael Hengge <mi.hengge@googlemail.com>
2025-01-27 13:12:54 +0100

    Update Docstring

--- DobotTCP.py
+++ DobotTCP.py
@@ -9,7 +9,7 @@ Author:
     Dr. Michael Hengge
 
 Version:
-    1.1.0 (20.01.2025)
+    1.1.1 (27.01.2025)
 
 Classes:
     Dobot: A class for controlling the Dobot robot arms using TCP/IP communication.
